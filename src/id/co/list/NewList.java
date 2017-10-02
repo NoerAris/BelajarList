6b346d0fbec2ef78dbb5bbe71fb8424ba731109f
Aris <Aris@Aris-PC>
2017-10-02 22:27:39 +0700

    Commit keempat

--- src/id/co/list/NewList.java
+++ src/id/co/list/NewList.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package id.co.hashmap;
+package id.co.list;
 
 /**
  *
  * @author Aris
  */
-public class Map {
-
+public class NewList {
+    
 }
